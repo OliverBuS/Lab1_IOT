cc736ebe6a01e3762b8ecfc3a107fa9de4e0bdb6
Labtel <labtel2022>
2022-09-02 09:43:10 -0500

    flecha de retorno añadida

--- app/src/main/java/edu/pe/pucp/lab1/activity_ter.java
+++ app/src/main/java/edu/pe/pucp/lab1/activity_ter.java
@@ -2,6 +2,7 @@ package edu.pe.pucp.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class activity_ter extends AppCompatActivity {
