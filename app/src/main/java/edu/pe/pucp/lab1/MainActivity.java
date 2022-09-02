feebf5a3ba111feab1358db041218bb13776046b
Labtel <labtel2022>
2022-09-02 08:59:32 -0500

    intents

--- app/src/main/java/edu/pe/pucp/lab1/MainActivity.java
+++ app/src/main/java/edu/pe/pucp/lab1/MainActivity.java
@@ -2,7 +2,9 @@ package edu.pe.pucp.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((Button) findViewById(R.id.btn_memoria)).setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, activity_mem.class);
+            startActivity(intent);
+        });
+
+
+        ((Button) findViewById(R.id.btn_ter)).setOnClickListener(view -> {
+            Intent intent1 = new Intent(MainActivity.this, activity_ter.class);
+            startActivity(intent1);
+        });
     }
 }
